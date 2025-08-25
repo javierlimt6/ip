6951b71d41eb726ebefe0b5bf29aa8702df320c7
Javier Lim <javierlimt6@gmail.com>
2025-08-25 16:14:04 +0800

    refactored greetings and goodbyes

--- src/main/java/Friday.java
+++ src/main/java/Friday.java
@@ -1,11 +1,43 @@
-public class Friday {
-    public static void main(String[] args) {
-        final String IND = "____________________________________________________________";
+import java.util.Scanner;
 
+public class Friday {
+    private static final String IND = "____________________________________________________________";
+
+    public static void main(String[] args) {
+
+        greet();
+        bye();
+    }
+
+    private static void indent() {
         System.out.println(IND);
+    }
+
+    private static void greet() {
+        indent();
         System.out.println(" Hello! I'm Friday");
         System.out.println(" What can I do for you?");
-        System.out.println(IND);
+        indent();
+    }
+
+    private static void bye() {
         System.out.println("Bye. Hope to see you again soon!");
+    }
+
+    private static void listen() {
+        Scanner in = new Scanner(System.in);
+        while (true) {
+            String line = in.nextLine().trim();
+            indent();
+            if ("bye".equals(line)) {
+                System.out.println(" Bye. Hope to see you again soon!");
+                indent();
+                break;
+            } else {
+                System.out.println(" " + line);
+                indent();
+            }
+        }
+        in.close();
     }
 }
